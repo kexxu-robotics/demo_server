c839e15ea2029e86d75c132524cf7b60fe0b740f
segfault <segfaul@pop-os.localdomain>
2023-04-14 17:54:36 +0300

    removed oauth

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,15 +6,6 @@ import { createAuth0 } from "@auth0/auth0-vue";
 
 const app = createApp(App);
 
-app.use(
-  createAuth0({
-    domain: process.env.AUTH0_DOMAIN,
-    clientId: process.env.AUTH0_CLIENT_ID,
-    authorizationParams: {
-      redirect_uri: window.location.origin
-    }
-  }),
-);
 app.use(router);
 
 app.mount('#app');
